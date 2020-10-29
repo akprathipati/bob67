54c0587c3430e4793e7d4570a7ef51c54394d2ab
Akshit Prathipati <akprathipati@gmail.com>
2020-10-29 20:57:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+def deriv():
+  #Dont need to add the x in it!
+  #Uses power rule 
+    coef=eval(input("Enter the coefficient : "))
+    ex=eval(input("Enter the exponent of the coefficient: "))
+    if coef<0 and ex<0:
+        print("The derivative is:", abs(coef*ex),"/(x^", abs((ex-1)),")")
+  
+    elif ex==1:
+        print("The derivative is:",coef)
+    elif ex>1:
+        print("The derivative is:", coef*ex,"x^", ex-1)
+    
+    elif coef<0:
+        print("The derivative is:", coef*ex,"x^", ex-1)
+    elif ex==0:
+        print("The derivative is: 0, obv smh")
+    
+    elif ex<0:
+        print("The derivative is:",coef*ex,"/(x^",abs(ex-1),")")
+deriv()
+#ak is cool 
